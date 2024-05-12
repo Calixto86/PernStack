1f6a9cbb94f758e57b93dd4482250c56b097fc47
Calixto86 <calixtomamani972@gmail.com>
2024-05-12 13:05:10 -0400

    elimnando tareas con DELETE

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,38 +1,64 @@
 const pool = require('../db')
 
-const getAllTasks = async(req, res) => {
-    res.send('retornando una lista de tareas')
+const getAllTasks = async (req, res) => {
+    try {
+        const alltasks = await pool.query('SELECT * FROM task')
+        res.json(alltasks.rows)
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-const getTask = (req, res) => {
-    res.send('retornando una sola tarea');
-}
+const getTask = async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
+
+        if (result.rows.length === 0)
+            return res.status(404).json({
+                message: "task not found",
+            });
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
+};
 
 const createTask = async (req, res) => {
     const { title, description } = req.body;
 
     try {
-        const result = await pool.query('INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *', 
-        [title, description,]
+        const result = await pool.query('INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *',
+            [title, description,]
         );
-    
+
         res.json(result.rows[0]);
-    }catch(error){
+    } catch (error) {
         console.log(error.message)
         res.json({ error: error.message })
     }
 }
 
-const deleteTask = (req, res) => {
-    res.send('eliminando una tareas');
+const deleteTask = async (req, res) => {
+    const { id } = req.params
+
+    const result = await pool.query('DELETE FROM task WHERE id = $1', [id]);
+
+    if(result.rowCount === 0) 
+        return res.status(404).json({
+            message: "task not found",
+    });
+
+    return res.sendStatus(204); //codigo de hhtp 204: todo fue bien pero el server no responde nada
 }
 
 const updateTask = (req, res) => {
-    res.send('actualizando una tareas');
+    res.send('eliminando una tareas');
 }
 
 module.exports = {
-    getAllTasks, 
+    getAllTasks,
     getTask,
     createTask,
     deleteTask,
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -5,13 +5,12 @@ const router = Router();
 
 router.get('/tasks', getAllTasks);
 
+router.get('/tasks/:id', getTask);
 
-router.get('/tasks/10', getTask)
+router.post('/tasks', createTask);
 
-router.post('/tasks', createTask)
+router.delete('/tasks/:id', deleteTask);
 
-router.delete('/tasks', deleteTask)
-
-router.put('/tasks',updateTask)
+router.put('/tasks',updateTask);
 
 module.exports = router;
