a94c91fc1701fe5cc12563a24364db9a76365bf5
Calixto86 <calixtomamani972@gmail.com>
2024-05-13 13:13:52 -0400

    creando formulario en TaskForm y manejo de estados

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,8 +1,69 @@
+import { Button, Card, CardContent, Grid, TextField, Typography, styled } from '@mui/material';
+import { useState, useEffect } from 'react'; //{capturar un estado, }
 
 export default function TaskForm() {
+
+    const [task, setTask] = useState({
+        title: '',
+        description: '',
+    });
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(task);
+    }
+
+    const handleChange = e => { {/*actualiza los cambios del estado*/}
+        setTask({...task, [e.target.name]: e.target.value})
+    };
+
     return (
-        <div>
-            TaskForm
-        </div>
+        <Grid container direction='column' alignItems='center' justifyContent='center'>{/*para que el recuadro este centrado ver, hori*/}
+            <Grid item xs={3}> {/*va ocupar 3 columnas por defecto es 12*/}
+                <Card sx={{ mt: 5 }} style={{
+                    backgroundColor: '#1e272e',
+                    padding: '1rem'
+                }}
+                >{/*margi-top*/}
+                    <Typography variant='5' textAlign='center' color='white'>Create Task</Typography>
+                    <CardContent>
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                variant='filled'
+                                label='Write your title'
+                                sx={{
+                                    display: 'block',
+                                    margin: '.5rem 0'
+                                }}
+                                name='title'
+                                onChange={handleChange}
+                                inputProps={{style: {color: 'white'}}}
+                                InputLabelProps={{style: {color: 'white'}}}
+                            />{/*inputs en material ui son TextField y label tiene animacion*/}
+
+                            <TextField
+                                variant='filled'
+                                label='Write your description'
+                                multiline
+                                rows={4}
+                                sx={{
+                                    display: 'block',
+                                    margin: '.5rem 0'
+                                }}
+                                name='description'
+                                onChange={handleChange}
+                                inputProps={{style: {color: 'white'}}}
+                                InputLabelProps={{style: {color: 'white'}}}
+                            />
+
+                            <Button variant='contained' color='primary' type='submit'> {/*submit para q se ejecute el formulario es decir lo refresca*/}
+                                Save
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     )
 }
