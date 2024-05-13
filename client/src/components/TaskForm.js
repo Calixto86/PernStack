1086abb36afde9c15f14689f50d74a86af8ead5e
Calixto86 <calixtomamani972@gmail.com>
2024-05-13 15:28:42 -0400

    creando lista de tareas y enlistado desde la base de datos

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -3,86 +3,86 @@ import { useState, useEffect } from 'react'; //{capturar un estado, }
 import { useNavigate } from 'react-router-dom';
 
 export default function TaskForm() {
-    const [task, setTask] = useState({
-        title: '',
-        description: '',
-    });
-    const [loading, setLoading] = useState(false);
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+  });
+  const [loading, setLoading] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        setLoading(true)
+    setLoading(true)
 
-        const res = await fetch("http://localhost:4000/tasks", {//envia una peticion get por defecto 
-            method: 'POST',
-            body: JSON.stringify(task), //convierte el objeto JSON en string
-            headers: { 'content-type': 'application/json' },//le mandamos en la cabecera http el tipo de recurso
-        })
-        const data = await res.json()//respuesta de la peticion http
+    const res = await fetch("http://localhost:4000/tasks", {//envia una peticion get por defecto 
+      method: 'POST',
+      body: JSON.stringify(task), //convierte el objeto JSON en string
+      headers: { 'content-type': 'application/json' },//le mandamos en la cabecera http el tipo de recurso
+    })
+    const data = await res.json()//respuesta de la peticion http
 
-        setLoading(false)
-        navigate('/') //lo redirecciona al inicio
-    }
+    setLoading(false)
+    navigate('/') //lo redirecciona al inicio
+  }
 
-    const handleChange = e => {
-        {/*actualiza los cambios del estado*/ }
-        setTask({ ...task, [e.target.name]: e.target.value })
-    };
+  const handleChange = e => {
+    {/*actualiza los cambios del estado*/ }
+    setTask({ ...task, [e.target.name]: e.target.value })
+  };
 
-    return (
-        <Grid container direction='column' alignItems='center' justifyContent='center'>{/*para que el recuadro este centrado ver, hori*/}
-            <Grid item xs={3}> {/*va ocupar 3 columnas por defecto es 12*/}
-                <Card sx={{ mt: 5 }} style={{
-                    backgroundColor: '#1e272e',
-                    padding: '1rem'
+  return (
+    <Grid container direction='column' alignItems='center' justifyContent='center'>{/*para que el recuadro este centrado ver, hori*/}
+      <Grid item xs={3}> {/*va ocupar 3 columnas por defecto es 12*/}
+        <Card sx={{ mt: 5 }} style={{
+          backgroundColor: '#1e272e',
+          padding: '1rem'
+        }}
+        >{/*margi-top*/}
+          <Typography variant='5' textAlign='center' color='white'>Create Task</Typography>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                variant='filled'
+                label='Write your title'
+                sx={{
+                  display: 'block',
+                  margin: '.5rem 0'
                 }}
-                >{/*margi-top*/}
-                    <Typography variant='5' textAlign='center' color='white'>Create Task</Typography>
-                    <CardContent>
-                        <form onSubmit={handleSubmit}>
-                            <TextField
-                                variant='filled'
-                                label='Write your title'
-                                sx={{
-                                    display: 'block',
-                                    margin: '.5rem 0'
-                                }}
-                                name='title'
-                                onChange={handleChange}
-                                inputProps={{ style: { color: 'white' } }}
-                                InputLabelProps={{ style: { color: 'white' } }}
-                            />{/*inputs en material ui son TextField y label tiene animacion*/}
+                name='title'
+                onChange={handleChange}
+                inputProps={{ style: { color: 'white' } }}
+                InputLabelProps={{ style: { color: 'white' } }}
+              />{/*inputs en material ui son TextField y label tiene animacion*/}
 
-                            <TextField
-                                variant='filled'
-                                label='Write your description'
-                                multiline
-                                rows={4}
-                                sx={{
-                                    display: 'block',
-                                    margin: '.5rem 0'
-                                }}
-                                name='description'
-                                onChange={handleChange}
-                                inputProps={{ style: { color: 'white' } }}
-                                InputLabelProps={{ style: { color: 'white' } }}
-                            />
+              <TextField
+                variant='filled'
+                label='Write your description'
+                multiline
+                rows={4}
+                sx={{
+                  display: 'block',
+                  margin: '.5rem 0'
+                }}
+                name='description'
+                onChange={handleChange}
+                inputProps={{ style: { color: 'white' } }}
+                InputLabelProps={{ style: { color: 'white' } }}
+              />
 
-                            <Button
-                                variant='contained'
-                                color='primary'
-                                type='submit'
-                                disabled={!task.title || !task.description}
-                            > {/*submit para q se ejecute el formulario es decir lo refresca*/}
-                                {loading ? (<CircularProgress color='inherit' size={24}/>) : 'Create'}
-                            </Button>
-                        </form>
-                    </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
-    )
+              <Button
+                variant='contained'
+                color='primary'
+                type='submit'
+                disabled={!task.title || !task.description}
+              > {/*submit para q se ejecute el formulario es decir lo refresca*/}
+                {loading ? (<CircularProgress color='inherit' size={24} />) : 'Create'}
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
 }
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,7 +1,50 @@
+import { useEffect, useState } from 'react';
+import { Button, Card, CardContent, Typography } from '@mui/material'
+
 export default function TaskList() {
-    return (
-        <div>
-            TaskList
-        </div>
-    )
+
+  const [tasks, setTasks] = useState([])
+
+  const loadTasks = async () => {
+    const response = await fetch('http://localhost:4000/tasks')
+    const data = await response.json()
+    setTasks(data)
+  }
+
+  useEffect(() => {
+    loadTasks()
+  }, [])
+
+  return (
+    <>
+      <h1>Task List</h1>
+      {
+        tasks.map(task => (
+          <Card style={{
+            marginBottom: ".7rem",
+            backgroundColor: '#1e272e'
+          }}> {/*creamos una tarjeta por cada tarea */}
+            <CardContent style={{
+              display: "flex",
+              justifyContent: "space-between"
+            }}>
+              <div style={{color: 'white'}}>
+                <Typography>{task.title}</Typography>
+                <Typography>{task.description}</Typography>
+              </div>
+
+              <div>
+                <Button variant='contained' color='inherit' onClick={() => console.log('Editando')}>
+                  Edit
+                </Button>
+                <Button variant='contained' color='warning' onClick={() => console.log('eliminando')} style={{marginLeft: '.5rem'}}>
+                  Delete
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        ))
+      }
+    </>
+  )
 }
